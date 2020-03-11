46213050cb3f94ff5d965b408c1509928e12fcd2
ShawTim <shawtim.liu@gmail.com>
2020-03-11 08:55:01 +0800

    add total row

--- src/js/main.js
+++ src/js/main.js
@@ -2,15 +2,15 @@ import numeral from "numeral";
 import populations from "./population.json";
 import { getData } from "./api";
 
-const totalNum = (data, field) => data.reduce((sum, d) => sum + d[field], 0);
+const totalNum = (data, field) => data.reduce((sum, d) => sum + (d[field] || 0), 0);
 
 const totalRate = (data, field) => {
   const total = data.reduce((sum, d) => sum + d[field], 0);
-  const totalPopulation = data.reduce((sum, d) => sum + d.population, 0);
+  const totalPopulation = data.reduce((sum, d) => sum + (d.population || 0), 0);
   return total/totalPopulation;
 }
 
-(async () => {
+$(async () => {
   const table = $("#data-table");
   const rawData = await getData();
   const tableData = rawData.filter((data) => data.Country_Region !== "Others").map((data) => {
@@ -29,32 +29,41 @@ const totalRate = (data, field) => {
   table.bootstrapTable({
     data: tableData,
     height: 800,
+    showFooter: true,
     columns: [{
       title: "排名", width: 50,
       formatter: (val, row, index) => index+1,
     }, {
       title: "國家/地區", field: "region", width: 300,
+      footerFormatter: (data) => "總計",
     }, {
-      title: "人口(萬)", field: "population", width: 100, align: "right", sortable: true,
+      title: "人口(萬)", field: "population", width: 150, align: "right", sortable: true,
       formatter: (val) => val ? numeral(val/10000).format("0,0") : "---",
+      footerFormatter: (data) => numeral(totalNum(data, "population")/10000).format("0,0"),
     }, {
       title: "感染人數", field: "confirmed", width: 100, align: "right", sortable: true,
       formatter: (val) => numeral(val).format("0,0"),
+      footerFormatter: (data) => numeral(totalNum(data, "confirmed")).format("0,0"),
     }, {
       title: "感染率(每萬)", field: "confirmedRate", width: 100, align: "right", sortable: true,
       formatter: (val, row) => row.population ? numeral(val*10000).format("0,0.000") : "---",
+      footerFormatter: (data) => numeral(totalRate(data, "confirmed")).format("0,0.000"),
     }, {
       title: "死亡人數", field: "death", width: 100, align: "right", sortable: true, 
       formatter: (val) => numeral(val).format("0,0"),
+      footerFormatter: (data) => numeral(totalNum(data, "death")).format("0,0"),
     }, {
       title: "死亡率", field: "deathRate", width: 100, align: "right", sortable: true,
       formatter: (val) => numeral(val).format("0.000%"),
+      footerFormatter: (data) => numeral(totalRate(data, "death")).format("0.000%"),
     }, {
       title: "康復人數", field: "recovered", width: 100, align: "right", sortable: true,
       formatter: (val) => numeral(val).format("0,0"),
+      footerFormatter: (data) => numeral(totalNum(data, "recovered")).format("0,0"),
     }, {
       title: "康復率", field: "recoveredRate", width: 100, align: "right", sortable: true,
       formatter: (val) => numeral(val).format("0.000%"),
+      footerFormatter: (data) => numeral(totalRate(data, "recovered")).format("0.000%"),
     }],
   });
-})();
+});
